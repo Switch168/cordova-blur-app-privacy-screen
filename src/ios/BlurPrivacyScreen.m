e001dc4fd4da478214139127f201d95823926411
momoterraw <andrewvmail@gmail.com>
2020-11-04 16:39:58 -0800

    Update BlurPrivacyScreen.m

--- src/ios/BlurPrivacyScreen.m
+++ src/ios/BlurPrivacyScreen.m
@@ -1,18 +1,26 @@
 #import "AppDelegate.h"
 
 @interface AppDelegate (BlurAppPrivacyScreen) {}
-
 @end
 
 @implementation AppDelegate (BlurAppPrivacyScreen)
 
+BOOL init;
+UIView *blurView;
+UIBlurEffect *blurEffect;
+UIVisualEffectView *blurEffectView;
+
+
 - (void)applicationWillResignActive:(UIApplication *)application {
     self.window.backgroundColor = [UIColor clearColor];
 
-    UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleExtraLight];
-    UIVisualEffectView *blurEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
-    blurEffectView.frame = self.window.bounds;
-    blurEffectView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    if(!init) {
+        blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleExtraLight];
+        blurEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
+        blurEffectView.frame = self.window.bounds;
+        blurEffectView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+        init = true;
+    }
 
     blurEffectView.tag = 1234;
     blurEffectView.alpha = 1;
@@ -22,20 +30,22 @@
     // fade in the view
     [UIView animateWithDuration:1.5 animations:^{
         blurEffectView.alpha = 1;
+    } completion:^(BOOL finished) {
+        [self.window bringSubviewToFront:blurEffectView];
     }];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    NSLog(@"Hiding blur");
-    // grab a reference to our custom blur view
-    UIView *blurEffectView = [self.window viewWithTag:1234];
+    blurEffectView = [self.window viewWithTag:1234];
+    [self.window bringSubviewToFront:blurEffectView];
 
     // fade away colour view from main view
     [UIView animateWithDuration:1.5 animations:^{
         blurEffectView.alpha = 0;
     } completion:^(BOOL finished) {
         // remove when finished fading
-        [blurEffectView removeFromSuperview];
+        [self.window sendSubviewToBack:blurEffectView];
+        blurEffectView.alpha = 1;
     }];
 }
 
